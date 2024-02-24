9f0f98c6daa5676dfe511f0b91a3494e4bd930bd
Leonardo Jacomussi <leonardojacomussi@gmail.com>
2024-02-24 16:12:05 -0300

    FIX: fixed avatar filename

--- src/controllers/UserAvatarController.js
+++ src/controllers/UserAvatarController.js
@@ -21,7 +21,7 @@ class UserAvatarController {
     };
 
     const filename = await diskStorage.saveFile(avatarFilename);
-    user.avatar = filename.error;
+    user.avatar = filename.file;
 
     await knex("users")
       .where("id", user_id)
